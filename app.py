e1ca6a269869878d34bb0a83a1e9e8bc015388e4
Hifiboombox <zbradley96@gmail.com>
2013-09-29 22:41:53 -0700

    added bootstrap, swag is no more, now uses input field

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 import web
 import info
-from random import choice
 
-urls = (
-  '/convert', 'Index'
-)
+URLS = ('/convert', 'Index')
 
-lilb = ['http://content.animalnewyork.com/wp-content/uploads/lil_b_grammys_blocked_gig.jpg',
-		'http://8tr.s3.amazonaws.com/i/000/460/481/Lil-B-9347.jpg',
-		'http://1.bp.blogspot.com/-QDpiWZs2sKI/UdmNiWIywPI/AAAAAAAAAa4/Rqldr4IVENs/s640/maxresdefault.jpg',
-		'http://25.media.tumblr.com/tumblr_m8xc441ezB1r899c9o1_1280.jpg']
-
-app = web.application(urls, globals())
-
+app = web.application(URLS, globals())
 render = web.template.render('pages/')
 
 class Index(object):
 	def GET(self):
+		return render.index()
+
+	def POST(self):
 		form = web.input(url=None)
-		swag = choice(lilb)
 		if form.url is not None:
-			response = info.get_song(form.url)
-			return render.index(response = response, swag = swag)
-		else:
-			return render.error(swag = swag)
+			response = info.song(form.url)
+			if not response:
+				return render.error()
+			else:
+				return render.response(response = response)
 
 if __name__ == "__main__":
 	app.run()
--- info.py
+++ info.py
@@ -7,30 +7,22 @@ with open('client.ini') as f:
 
 CLIENT_ID = client_info['client_id']
 CLIENT_SECRET = client_info['client_secret']
-client = soundcloud.Client(client_id=CLIENT_ID)
+CLIENT = soundcloud.Client(client_id=CLIENT_ID)
 
-def get_song(track_url):
-	url = track_url
-	r = requests.get('http://api.soundcloud.com/resolve.json?url=%s&client_id=%s' % (url, CLIENT_ID))
+def song(track):
+	r = requests.get('http://api.soundcloud.com/resolve.json?url=%s&client_id=%s' % (track, CLIENT_ID))
+	try:
+		id = r.json()['id']
+		title = r.json()['title']
+		artist = r.json()['user']['username']
+		hours = r.json()['duration'] / 3600000
+		minutes = r.json()['duration'] / 60000
+		seconds = int((((r.json()['duration']) / 60000.0) % 1) * 60)
+		length = "%s : %s : %s" % (hours, minutes, seconds)
+		url = CLIENT.get(CLIENT.get('/tracks/' + str(id)).stream_url, allow_redirects=False).location
+		json = r.json()
 
-	# general information
-	id = r.json()['id']
-	title = r.json()['title']
-	artist = r.json()['user']['username']
+		return {'id': id,'title': title,'artist': artist,'length': length,'url': url,'json': json}
 
-	# time information
-	hours = (r.json()['duration']) / 3600000
-	minutes = (r.json()['duration']) / 60000
-	seconds = int((((r.json()['duration']) / 60000.0) % 1) * 60)
-	length = "%s: %s: %s" % (hours, minutes, seconds)
-
-	# technical information
-	stream_url = client.get(client.get('/tracks/' + str(id)).stream_url, allow_redirects=False).location
-	json = r.json()
-
-	return {'id': id,
-			'title': title,
-			'artist': artist,
-			'length': length,
-			'stream_url': stream_url,
-			'json': json}
+	except KeyError:
+		return False
